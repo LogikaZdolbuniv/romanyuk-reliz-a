58fe3bea41d094b326a581dc52179271b3c50152
LogikaZdolbuniv <vv1ktoriababenko@gmail.com>
2026-04-18 11:15:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const products = [
     { id: 1, name: "Зволожуючий крем", price: 450, img: "https://images.unsplash.com/photo-1556228720-195a672e8a03?w=500" },
     { id: 2, name: "Сироватка з вітаміном C", price: 620, img: "https://images.unsplash.com/photo-1620916566398-39f1143ab7be?w=500" },
     { id: 3, name: "Олія для обличчя", price: 380, img: "https://images.unsplash.com/photo-1601049541289-9b1b7bbbfe19?w=500" },
-    { id: 4, name: "", price: 450, img: "" },
-    { id: 5, name: "", price: 450, img: "" },
-    { id: 6, name: "", price: 450, img: "" },
+    { id: 4, name: "Очищувальна пінка", price: 320, img: "https://images.unsplash.com/photo-1556228578-0d85b1a4d571?w=500" },
+    { id: 5, name: "Тонік з ніацинамідом", price: 480, img: "https://images.unsplash.com/photo-1617897903246-719242758050?w=500" },
+    { id: 6, name: "Сонцезахисний флюїд", price: 550, img: "https://images.unsplash.com/photo-1598440467723-5935f444de4e?w=500" },
 ];
 
 let cart = [];
